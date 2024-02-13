b3b6a03cdc6a32656465eb8f581d07112910f1d1
2karis <gitauk2@gmail.com>
2024-02-12 20:14:04 -0500

    updated repo

--- backend/src/main/java/io/siliconsavannah/backend/repo/ExpenseRepo.java
+++ backend/src/main/java/io/siliconsavannah/backend/repo/ExpenseRepo.java
@@ -6,7 +6,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface ExpenseRepo extends JpaRepository<Expense, Integer> {
-    void deleteExpenseById(int id);
-
-    Optional<Expense> findExpenseById(int id);
 }
--- backend/src/main/java/io/siliconsavannah/backend/repo/IncomeRepo.java
+++ backend/src/main/java/io/siliconsavannah/backend/repo/IncomeRepo.java
@@ -1,7 +1,10 @@
 package io.siliconsavannah.backend.repo;
 
+import io.siliconsavannah.backend.model.Expense;
 import io.siliconsavannah.backend.model.Income;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
 
 public interface IncomeRepo extends JpaRepository<Income, Integer> {
 }
