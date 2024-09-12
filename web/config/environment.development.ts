994d60a79a068302b0e80c34329ef3e7440108c9
2karis <gitauk2@gmail.com>
2024-09-12 13:13:10 -0400

    configured for prod

--- web/config/environment.development.ts
+++ web/config/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    appBaseUrl : 'http://localhost:80/api'
-    //appBaseUrl : 'http://161.35.251.91/api'
+    //appBaseUrl : 'http://localhost:80/api'
+    appBaseUrl : 'http://161.35.251.91/api'
 };
